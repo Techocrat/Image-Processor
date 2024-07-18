398f279014645796f796a5b4bc80cd27b2c1fef8
Techocrat <akarshpandey9599@gmail.com>
2024-07-19 01:05:02 +0530

    [ADDED] image and status routes

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
+app.use('/api/images', imageRoutes);
+app.use('/api', statusRoutes);
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
---
+++ routes/statusRoutes.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import { checkStatus } from '../controllers/statusController.js';
+const router = express.Router();
+
+router.get('/status/:requestId', checkStatus);
+
+export default router;
